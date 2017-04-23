dbb0803ef13d028ad77dec4224d22ecf2af16164
Ederson Abreu <ederson.dimm@gmail.com>
2017-04-23 18:01:12 -0300

    nome bucket

--- src/main/java/br/eti/qisolucoes/contactcloud/constants/AwsConstants.java
+++ src/main/java/br/eti/qisolucoes/contactcloud/constants/AwsConstants.java
@@ -4,9 +4,9 @@ public class AwsConstants {
 	
 	public static String S3_PREFIX = "s3://";
 	
-	public static String BUCKET_NAME = "contactcloudtest";
+	public static String BUCKET_NAME = "contactcloudstore";
 	
-	public static String PUBLIC_FOTO_PATH = "https://s3-sa-east-1.amazonaws.com/contactcloudtest/";
+	public static String PUBLIC_FOTO_PATH = "https://s3-sa-east-1.amazonaws.com/contactcloudstore/";
 	
 	public static String S3_DEFAULT_FILE = "defaultfile.png";
 
